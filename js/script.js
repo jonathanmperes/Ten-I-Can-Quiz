1d3f673bd2094607b2c7355a7520c37c3007d2d5
JonathanMPeres <emailmarceloperes@gmail.com>
2024-02-05 09:31:15 -0300

    declaring variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const question = document.querySelector("#question");
+const answersBox = document.querySelector("#answers-box");
+const quizContainer = document.querySelector("#quiz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters = ["a", "b", "c", "d"];
+let actualQuestion = 0;
+let point = 0;
