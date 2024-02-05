50151cadec122ca5f6fffb6c4a1aa7ca0466ce2a
JonathanMPeres <emailmarceloperes@gmail.com>
2024-02-05 09:39:25 -0300

    mock data

--- js/script.js
+++ js/script.js
@@ -5,3 +5,48 @@ const scoreContainer = document.querySelector("#score-container");
 const letters = ["a", "b", "c", "d"];
 let actualQuestion = 0;
 let point = 0;
+
+const questions = [
+    {
+        "question": "What _____ you do last night?",
+        "answer": [
+            {
+                "answer": "did",
+                "correct": true
+            },
+            {
+                "answer": "do",
+                "correct": false
+            },
+            {
+                "answer": "will",
+                "correct": false
+            },
+            {
+                "answer": "had",
+                "correct": false
+            },
+        ],
+    },
+    {
+        "question": "What _____ you do tomorrow?",
+        "answer": [
+            {
+                "answer": "did",
+                "correct": false
+            },
+            {
+                "answer": "do",
+                "correct": false
+            },
+            {
+                "answer": "will",
+                "correct": true
+            },
+            {
+                "answer": "had",
+                "correct": false
+            },
+        ],
+    }
+]
